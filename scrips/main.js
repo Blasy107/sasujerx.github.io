54e2b0fdcadaca96146f9c10e5f6c6544a712ed1
sasujerx <71850190+sasujerx@users.noreply.github.com>
2020-09-24 17:12:03 -0500

    Add files via upload

---
+++ scrips/main.js
@@ -0,0 +1,4 @@
+const marketingparaprincipiantes = document.querySelector('h1');
+marketingparaprincipiantes.textContent = '¡ME QUIERO CORTAR LA PIJA AHHHHHHH';
+let curso2='Gratuito';
+curso2='curso Gratuito';
